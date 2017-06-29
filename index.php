8a3abaee9aa93ee0e8cf6388370974ef8c835aea
Ian Nerney <iannerney@gmail.com>
2017-06-29 15:18:20 -0400

    Added Google Analytics

--- index.php
+++ index.php
@@ -19,6 +19,17 @@ require 'fetch-weather-data.php';
     <link rel="stylesheet" type="text/css" href="/css/responsive.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <script src="https://use.fontawesome.com/d726da7372.js"></script>
+    <!-- GOOGLE ANALYTICS -->
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-101848302-1', 'auto');
+    ga('send', 'pageview');
+
+    </script>
 </head>
 <body>
 
