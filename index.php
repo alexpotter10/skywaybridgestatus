6153f97190731778c2b52a554bc3dc85241f3b2f
Ian Nerney <iannerney@gmail.com>
2018-02-26 17:45:05 -0500

    Updates to CSS and page layout

--- index.php
+++ index.php
@@ -37,11 +37,8 @@ require 'fetch-weather-data.php';
             <div class="navbar__logo col-6">
                 Skyway Bridge Status
             </div>
-            <div class="navbar__social-icons col-6">
-                <ul>
-                    <a href="https://twitter.com/SkywayStatus" title="Skyway Bridge Status on Twitter" alt="Skyway Bridge Status Twitter link"><li class="fa fa-twitter-square" aria-hidden="true"></li></a>
-                    <a href="https://github.com/SkywayBridgeStatus" title="Skyway Bridge Status on GitHub" alt="Skyway Bridge Status GitHub link"><li class="fa fa-github-square" aria-hidden="true"></li></a>
-                <ul>    
+            <div class="navbar__navigation col-6">
+                <a href="mailto:info@skywaybridgestatus.com" title="Contact" alt="Contact Link">Contact</a>
             </div>
         </div>
     </div>
@@ -121,6 +118,10 @@ require 'fetch-weather-data.php';
             <div class="footer__copyright center col-12">
                 <p>SkywayBridgeStatus.com &copy; <?php echo date('Y');?> <a href="http://www.iannerney.com">Ian Nerney</a></p>
                 <p>This project is available for use and modification under the <a href="/LICENSE">MIT License</a>.</p>
+                <ul class="footer__social-icons">
+                    <a href="https://twitter.com/SkywayStatus" title="Skyway Bridge Status on Twitter" alt="Skyway Bridge Status Twitter link"><li class="fa fa-twitter-square" aria-hidden="true"></li></a>
+                    <a href="https://github.com/SkywayBridgeStatus" title="Skyway Bridge Status on GitHub" alt="Skyway Bridge Status GitHub link"><li class="fa fa-github-square" aria-hidden="true"></li></a>
+                <ul>    
             </div>
         </section>
     </div>
