d87ef6443604944519e33f2d7ccb1018b6e09262
Ian Nerney <iannerney@gmail.com>
2018-03-06 13:17:49 -0500

    Added terms of use

---
+++ terms-of-use.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Terms of Use | Skyway Bridge Status</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="Keywords" content="skyway bridge status, terms of use">
+    <meta name="Description" content="This outlines the terms of use for Skyway Bridge Status. If you wish to use our site, you must agreet to these terms.">
+    <link rel="canonical" href="https://www.skywaybridgestatus.com/terms-of-use/" />
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Libre+Franklin" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <script src="https://use.fontawesome.com/d726da7372.js"></script>
+    <!-- FAVICONS -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <!-- GOOGLE ANALYTICS -->
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-101848302-1', 'auto');
+    ga('send', 'pageview');
+
+    </script>
+</head>
+<body>
+
+<?php include 'template-parts/header.php';?>
+
+<div class="main">
+    <div class="container">
+        <section class="terms col-12">
+
+            <h2>
+                Terms and Conditions of Use
+            </h2>
+
+            <h3>
+                1. Terms
+            </h3>
+
+            <p>
+                By accessing this web site, you are agreeing to be bound by these 
+                web site Terms and Conditions of Use, all applicable laws and regulations, 
+                and agree that you are responsible for compliance with any applicable local 
+                laws. If you do not agree with any of these terms, you are prohibited from 
+                using or accessing this site. The materials contained in this web site are 
+                protected by applicable copyright and trade mark law.
+            </p>
+
+            <h3>
+                2. Use License
+            </h3>
+
+            <p>
+                The <a href="/LICENSE">MIT License</a> applies to the use and distribution of this software.
+
+                <blockquote>
+
+                    <p>
+                    Copyright © <?php echo date("Y");?> Ian Nerney
+                    </p>
+                    <p>
+                    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+                    </p>
+                    <p>
+                    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+                    </p>
+                    <p>
+                    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+                </blockquote>
+
+            <h3>
+                3. Disclaimer
+            </h3>
+
+            <ol type="a">
+                <li>
+                    The materials on this web site are provided "as is". Ian Nerney and related parties make no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, Ian Nerney and related parties do not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on this Internet web site or otherwise relating to such materials or on any sites linked to this site.
+                </li>
+            </ol>
+
+            <h3>
+                4. Limitations
+            </h3>
+
+            <p>
+                In no event shall Ian Nerney or related parties be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on Ian Nerney's Internet site, even if Ian Nerney or a authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
+            </p>
+                        
+            <h3>
+                5. Revisions and Errata
+            </h3>
+
+            <p>
+                The materials appearing on Ian Nerney's web site could include technical, typographical, or photographic errors. Ian Nerney does not warrant that any of the materials on its web site are accurate, complete, or current. Ian Nerney may make changes to the materials contained on its web site at any time without notice. Ian Nerney does not, however, make any commitment to update the materials.
+            </p>
+
+            <h3>
+                6. Links
+            </h3>
+
+            <p>
+                Ian Nerney has not reviewed all of the sites linked to its Internet web site and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by Ian Nerney of the site. Use of any such linked web site is at the user's own risk.
+            </p>
+
+            <h3>
+                7. Site Terms of Use Modifications
+            </h3>
+
+            <p>
+                Ian Nerney may revise these terms of use for its web site at any time without notice. By using this web site you are agreeing to be bound by the then current version of these Terms and Conditions of Use.
+            </p>
+
+            <h3>
+                8. Governing Law
+            </h3>
+
+            <p>
+                Any claim relating to Ian Nerney's web site shall be governed by the laws of the State of Florida without regard to its conflict of law provisions.
+            </p>
+
+        </section>
+
+        <?php include 'template-parts/footer.php';?>
+
+    </div>
+</div>
+</body>
+</html>
