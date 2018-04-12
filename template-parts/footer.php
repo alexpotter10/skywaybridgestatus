12f5a872888be17494345393115cdcdac20a1847
Ian Nerney <iannerney@gmail.com>
2018-04-12 17:21:05 -0400

    Added Facebook link and pixel

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -4,6 +4,7 @@
         By using this web site you agree to the <a href="/terms-of-use/">terms of use</a>.</p>
         <ul class="footer__social-icons">
             <a href="https://twitter.com/SkywayStatus" title="Skyway Bridge Status on Twitter" alt="Skyway Bridge Status Twitter link"><li class="fa fa-twitter-square" aria-hidden="true"></li></a>
+            <a href="https://www.facebook.com/SkywayStatus/" title="Skyway Bridge Status on Facebook" alt="Skyway Bridge Status Facebook link"><li class="fa fa-facebook-square" aria-hidden="true"></li></a>
             <a href="https://github.com/SkywayBridgeStatus" title="Skyway Bridge Status on GitHub" alt="Skyway Bridge Status GitHub link"><li class="fa fa-github-square" aria-hidden="true"></li></a>
         <ul>    
     </div>
