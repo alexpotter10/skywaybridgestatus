a2b2e3b35e40e985ca5c5183ce9ab4a874a4603d
Ian Nerney <iannerney@gmail.com>
2018-05-31 10:56:06 -0400

    Add affiliate banner to site and supporting css changes

--- index.php
+++ index.php
@@ -48,11 +48,11 @@ require 'fetch-planned-closures.php';
                 <h1>The Sunshine Skyway Bridge is currently 
                 <br/>
                 <span class="status--<?php echo $global_status_modifier;?>"><?php echo $global_status_string;?></span></h1>
-                <p><a href=".">Refresh this page</a> for the latest data. We update our status every 5 minutes.</p>
+                <p><a href="." onclick="ga('send', 'event', 'Status Refresh', 'Link Click', 'Homepage Refresh');">Refresh this page</a> for the latest data. We update our status every 5 minutes.</p>
                 <p class="status__data-refreshed">(Status last updated: <?php echo date_format($fetch_datetime,'F jS, Y \a\t g:ia');?>)</p>
             </div>
             <div class="status__facebook-like-button">
-                <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.skywaybridgestatus.com%2F&width=140&layout=button_count&action=like&size=large&show_faces=true&share=true&height=46&appId=1853913434627398" width="140" height="46" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+                <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.skywaybridgestatus.com%2F&width=140&layout=button_count&action=like&size=large&show_faces=true&share=true&height=46&appId=1853913434627398" width="150" height="46" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
             </div>
         </section>
 
@@ -66,13 +66,29 @@ require 'fetch-planned-closures.php';
         
         ?>
 
-        <section class="announcements col-12">
-            <div class="announcements-box">
-                <b>PLANNED CLOSURE ALERT:</b> <?php echo $planned_closure["closed_direction"]; ?> traffic on the Skyway Bridge will be suspended from <?php echo date_format($start_date,'g:ia \o\n l F jS, Y'); ?> to <?php echo date_format($end_date,'g:ia \o\n l F jS, Y'); ?> for <?php echo $planned_closure["reason"]; ?>. <a href="<?php echo $planned_closure["url"]; ?>"> Learn more.</a>
+        <section class="announcements row">
+            <div class="announcements__column col-12">
+                <div class="announcements-box">
+                    <b>PLANNED CLOSURE ALERT:</b> <?php echo $planned_closure["closed_direction"]; ?> traffic on the Skyway Bridge will be suspended from <?php echo date_format($start_date,'g:ia \o\n l F jS, Y'); ?> to <?php echo date_format($end_date,'g:ia \o\n l F jS, Y'); ?> for <?php echo $planned_closure["reason"]; ?>. <a href="<?php echo $planned_closure["url"]; ?>"> Learn more.</a>
+                </div>
             </div>
         </section>
 
         <?php endforeach; ?>
+
+        <section class="ad-main row">
+            <div class="ad-main__content col-12">
+                <div class="ad-main__mobile">
+                    <iframe src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=12&l=ez&f=ifr&linkID=e2176811beea9c4c4b07f522188a0500&t=leewardintera-20&tracking_id=leewardintera-20" width="300" height="250" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
+                </div>
+                <div class="ad-main__desktop">
+                    <iframe src="//rcm-na.amazon-adsystem.com/e/cm?o=1&p=48&l=ez&f=ifr&linkID=4aacc3c69fcb1f9af4ef0ab44bec70cf&t=leewardintera-20&tracking_id=leewardintera-20" width="728" height="90" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0"></iframe>
+                </div>
+                <div class="ads__explanation">
+                    <a href="/advertising-policy/">Why Ads?</a> | <a href="mailto:contact@skywaybridgestatus.com?subject=Advertising Information Request">Advertise With Us</a>
+                </div>
+            </div>
+        </section>
 
         <section class="data-sources row">
             <div class="data-sources__status col-6">
