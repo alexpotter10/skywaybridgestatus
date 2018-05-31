d275fccd06794387c46d9fd59321f93446bffac0
Ian Nerney <iannerney@gmail.com>
2018-05-30 22:16:36 -0400

    Refactor planned closure query and add caching

--- fetch-planned-closures.php
+++ fetch-planned-closures.php
@@ -3,19 +3,16 @@
 require 'config.php';
 
 try {
-    // Set current date for SQL query
-    $date = new DateTime('now');
-
     // Prepare SQL query
     $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT * FROM planned_closures pc WHERE pc.end_datetime > CURDATE() ORDER BY pc.start_datetime DESC LIMIT 1;"); 
+    $stmt = $conn->prepare("/*" . MYSQLND_QC_ENABLE_SWITCH . "*/" . "SELECT * FROM planned_closures pc WHERE pc.end_datetime > CURDATE() ORDER BY pc.start_datetime DESC LIMIT 1;"); 
     
     $stmt->execute();
 
     // set the resulting array to associative
     $planned_closure = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
+
 }
 
 catch(PDOException $e) {
