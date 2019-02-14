cb648d48ff5dd10b9005b363fe51ec25653fec23
Ian Nerney <ian@savvycard.com>
2019-02-14 14:20:09 -0500

    Merged/squashed public website into new repo

---
+++ public/advertising-policy.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Advertising Policy | Skyway Bridge Status</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="Keywords" content="skyway bridge status, advertising policy">
+    <meta name="Description" content="This page outlines the advertising policy for Skyway Bridge Status. ">
+    <link rel="canonical" href="https://www.skywaybridgestatus.com/terms-of-use/" />
+    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Libre+Franklin" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+    <script src="https://use.fontawesome.com/d726da7372.js"></script>
+    <!-- FAVICONS -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+    <?php include 'template-parts/tracking.php';?>
+</head>
+<body>
+
+<?php include 'template-parts/header.php';?>
+
+<div class="main">
+    <div class="container">
+        <section class="advertising-policy col-12">
+
+            <h1>Why Ads?</h1>
+
+            <em>Updated May 30th, 2018</em>
+
+            <p>To help keep this site free for our users, we use display advertising and sponsorships to pay for the costs associated with the development and maintenance of this site. However, we know that advertising can sometimes be a contentious topic, and have prepared the following policy around our use of advertising.</p>
+
+            <p>If you have any questions about this policy, or would like to report issues with our use of advertising on this website, please contact us at <a href="mailto:contact@skywaybridgestatus.com">contact@skywaybridgestatus.com</a>.</p>
+
+            <h2>Our Advertising Policy</h2>
+
+            <ol>
+
+            <li><b>We will not sell your personal details onto a third party</b> – Advertisers on our site may have access to your behavior via cookies, but not your data. Please see our <a href="/terms-of-use">terms of use</a> for more information on this topic.</li>
+
+            <li><b>We place ads for both optimal ad performance and user experience</b> – It is our goal to help subsidize the development and maintenance costs of this website, but to do so in a way that does not take away from our mission of providing useful information to our users.</li>
+
+            <li><b>We will not use behavioural targeting unreasonably</b> – We utilize third-party ad delivery platforms to show targeted advertising to you. Please note you may opt-out of this targeted advertising by blocking the use of cookies on your web browser.</li>
+
+            <li><b>We request that you don’t use ad blockers</b> – If you like our site, please don’t block our ads. Ads are an easy way for you to support us without it costing you anything.</li>
+
+            <li><b>We will take reasonable efforts to block offensive and malicious advertisements</b> – If you have a complaint about an ad for any reason, please get in touch with us and let us know and we will do what we can about it. It is not in our interest to displease our users!</li>
+
+            </ol>
+
+
+        </section>
+
+        <?php include 'template-parts/footer.php';?>
+
+    </div>
+</div>
+</body>
+</html>
---
+++ public/template-parts/styles-and-fonts.php
@@ -0,0 +1,10 @@
+<!-- Preload important assets for performance -->
+<link rel="preload" href="/assets/css/style.css" as="style" type="text/css">
+<link rel="preload" href="/assets/fonts/libre-franklin.woff2" as="font" type="font/woff2">
+<link rel="preload" href="/assets/fonts/roboto.woff2" as="font" type="font/woff2">
+
+<!-- Link to page stylesheets -->
+<link rel="stylesheet" href="/assets/css/style.css" type="text/css">
+
+<!-- FontAwesome script -->
+<script src="https://use.fontawesome.com/d726da7372.js"></script>
