031eb3b72e1dbfa343155d1c26335a7cedebbbf1
Ian Nerney <ian@savvycard.com>
2019-03-03 04:02:41 -0500

    Add check to ensre updates are coming from the SBS mailing list

--- public/webhooks/mailchimp.php
+++ public/webhooks/mailchimp.php
@@ -21,46 +21,52 @@ if($_GET["access_key"] !== $mailchimp_access_key) {
     echo "You have access!";
 
     // Receive Mailchimp webhook event
-    $event_data = Webhook::receive();
+    $event = Webhook::receive();
 
-    try {
-        // Set metadata variables
-        //$endpoint_url = "https://www.skywaybridgestatus.com/webhooks/mailchimp.php";
-        $event_time = time();
-        $event_status = "new";
-        $agent = $_SERVER['HTTP_USER_AGENT'];
+    // Ensure event is for the SBS list
+    $list_id = $event->{"data"}->{"list_id"};
+    if ($list_id = "b3f668b8e6") {
 
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        // Set the PDO error mode to exception
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // Log webhook event to database
+        try {
+            // Set metadata variables
+            $event_data = json_encode($event); //
+            $event_time = time();
+            $event_status = "new";
+            $agent = $_SERVER['HTTP_USER_AGENT'];
 
-        // Prepare SQL and bind parameters
-        $stmt = $conn->prepare(
-        "INSERT INTO mailchimp_webhook_log (event_data, event_time, event_status, agent)
-        VALUES (:event_data, :event_time, :event_status, :agent);");
+            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+            // Set the PDO error mode to exception
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $stmt->bindParam(':event_data', $event_data);
-        $stmt->bindParam(':event_time', $event_time);
-        $stmt->bindParam(':event_status', $event_status);
-        $stmt->bindParam(':agent', $agent);
+            // Prepare SQL and bind parameters
+            $stmt = $conn->prepare(
+            "INSERT INTO mailchimp_webhook_log (event_data, event_time, event_status, agent)
+            VALUES (:event_data, :event_time, :event_status, :agent);");
 
-        // Statement has been defined; execute!
-        $stmt->execute();
+            $stmt->bindParam(':event_data', $event_data);
+            $stmt->bindParam(':event_time', $event_time);
+            $stmt->bindParam(':event_status', $event_status);
+            $stmt->bindParam(':agent', $agent);
 
-        // Close the connection
-        $conn = null;
+            // Statement has been defined; execute!
+            $stmt->execute();
 
+            // Close the connection
+            $conn = null;
+
+        }
+        catch(PDOException $e) {
+            echo "Error: " . $e->getMessage();
+
+            // Send error to Sentry
+            $sentryClient->captureException($e, array(
+                'extra' => array(
+                    'php_version' => phpversion()
+                ),
+            ));
+        }
     }
-    catch(PDOException $e) {
-        echo "Error: " . $e->getMessage();
-
-        // Send error to Sentry
-        $sentryClient->captureException($e, array(
-            'extra' => array(
-                'php_version' => phpversion()
-            ),
-        ));
-    }
-};
+}
 
 ?>
